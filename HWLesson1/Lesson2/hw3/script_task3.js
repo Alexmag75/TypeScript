dde82dcb0b7eb6685c2028b891c2fd95c23b69aa
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-12-07 17:59:04 +0200

    Commit start

--- HWLesson1/Lesson2/hw3/script_task3.js
+++ HWLesson1/Lesson2/hw3/script_task3.js
@@ -1,20 +1,22 @@
-let book1={
+"use strict";
+let book3_1 = {
     title: 'title_1',
     pageCount: 155,
     genre: 'fantasy',
-    authors:['Alex','50']
+    authors: ['Alex', '50']
 };
-let book2={
+let book3_2 = {
     title: 'title_2',
     pageCount: 175,
     genre: 'action_movie',
-    authors:['Anton','35']
+    authors: ['Anton', '35']
 };
-let book3={
+let book3_3 = {
     title: 'title_3',
     pageCount: 655,
     genre: 'novel',
-    authors:['Sem','38']
+    authors: ['Sem', '38']
 };
-
-console.log(book1);
+console.log(book3_1);
+console.log(book3_2);
+console.log(book3_3);
---
+++ HWLesson1/Lesson2/hw3/script_task3.ts
@@ -0,0 +1,22 @@
+let book3_1:{title:string; pageCount:number;genre:string;authors:string[]} = {
+    title: 'title_1',
+    pageCount: 155,
+    genre: 'fantasy',
+    authors:['Alex','50']
+};
+let book3_2:{title:string; pageCount:number;genre:string;authors:string[]}={
+    title: 'title_2',
+    pageCount: 175,
+    genre: 'action_movie',
+    authors:['Anton','35']
+};
+let book3_3:{title:string; pageCount:number;genre:string;authors:string[]}={
+    title: 'title_3',
+    pageCount: 655,
+    genre: 'novel',
+    authors:['Sem','38']
+};
+
+console.log(book3_1);
+console.log(book3_2);
+console.log(book3_3);
