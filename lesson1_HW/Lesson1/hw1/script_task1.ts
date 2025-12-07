d81d6b4ffac0709752d62efd8fbbcf174f1f8884
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-12-07 15:15:54 +0200

    start

---
+++ lesson1_HW/Lesson1/hw1/script_task1.ts
@@ -0,0 +1,27 @@
+let hello:string='Hello';
+let owu:string='owu';
+let com:string='com';
+let ua:string='ua';
+let num_1:number=1;
+let num_2:number=10;
+let num_3:number=-999;
+let num_4:number=123;
+let num_5:number=3.14;
+let num_6:number=2.7;
+let num_7:number=16;
+let bl_1:boolean=true;
+let bl_2:boolean=false;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(num_1);
+console.log(num_2);
+console.log(num_3);
+console.log(num_4);
+console.log(num_5);
+console.log(num_6);
+console.log(num_7);
+console.log(bl_1);
+console.log(bl_2);
