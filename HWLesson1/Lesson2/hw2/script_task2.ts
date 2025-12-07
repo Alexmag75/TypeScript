3a771115a7af7110d6f5a537ada20283b16f4968
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-12-07 17:50:12 +0200

    Commit start

--- HWLesson1/Lesson2/hw2/script_task2.ts
+++ HWLesson1/Lesson2/hw2/script_task2.ts
@@ -1,14 +1,14 @@
-let book1={
+let book1:{title:string; pageCount:number;genre:string}={
     title: 'title_1',
     pageCount: 155,
     genre: 'fantasy'
 };
-let book2={
+let book2:{title:string; pageCount:number;genre:string}={
     title: 'title_2',
     pageCount: 175,
     genre: 'action_movie'
 };
-let book3={
+let book3:{title:string; pageCount:number;genre:string}={
     title: 'title_3',
     pageCount: 655,
     genre: 'novel'
