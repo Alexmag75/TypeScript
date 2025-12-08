9d7b239e45ecf923b105631f52c8f3cf5dd8c2d2
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-12-08 10:55:56 +0200

    Commit start

---
+++ HWLesson1/Lesson2/hw5/script_task5.ts
@@ -0,0 +1,43 @@
+let temperature_week_1={
+    dey_1:[10,15,11],
+    dey_2:[11,15,10],
+    dey_3:[13,13,11],
+    dey_4:[14,15,12],
+    dey_5:[9,14,14],
+    dey_6:[10,16,9],
+    dey_7:[11,18,12]
+}
+type Temperature_week_2Type={morning:number;dey:number;evening:number}
+
+let temperature_week_2: Temperature_week_2Type[]=[
+    { morning:10,
+        dey:15,
+        evening:11
+    },
+    { morning:10,
+        dey:15,
+        evening:11
+    },
+    { morning:10,
+        dey:15,
+        evening:11
+    },
+    { morning:10,
+        dey:15,
+        evening:11
+    },
+    { morning:10,
+        dey:15,
+        evening:11
+    },
+    { morning:10,
+        dey:15,
+        evening:11
+    },
+    {
+        morning: 10,
+        dey: 15,
+        evening: 11
+    }
+]
+
