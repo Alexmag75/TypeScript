5f23dd563f7d5ed8e4b16349da916b123aa20da0
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-12-08 14:32:07 +0200

    Commit start

---
+++ HWLesson1/Lesson2/hw10/script_task10.js
@@ -0,0 +1,11 @@
+"use strict";
+let number_1 = Number(prompt('Enter number 1'));
+let number_2 = Number(prompt('Enter number 2'));
+if (number_1 > number_2) {
+    console.log('max=', number_1);
+}
+else if (number_1 < number_2) {
+    console.log('max=', number_2);
+}
+else
+    console.log('number1 = number_2');
---
+++ HWLesson1/Lesson2/hw11/script_task11.js
@@ -0,0 +1,6 @@
+"use strict";
+let number = prompt('Enter number');
+if (!number) {
+    number = 'default';
+}
+console.log(number);
