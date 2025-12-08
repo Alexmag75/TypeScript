8dc2f58e92276388016aa88a7e351cb6de1f1041
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-12-08 22:05:31 +0200

    Commit start

--- HWLesson1/Lesson4/hw1/script_task1.js
+++ HWLesson1/Lesson4/hw1/script_task1.js
@@ -1,7 +1,8 @@
-let a=2;
-let b = 5;
-function area(a,b){
-    return a*b;
+"use strict";
+let num1 = 2;
+let num2 = 5;
+function area(a, b) {
+    return a * b;
 }
-let s=area(a,b);
+let s = area(num1, num2);
 console.log(s);
---
+++ HWLesson1/Lesson4/hw1/script_task1.ts
@@ -0,0 +1,7 @@
+let num1:number=2;
+let num2:number=5;
+function area(a:number,b:number):number {
+    return a*b;
+}
+let s:number=area(num1,num2);
+console.log(s);
