d2ef2a4c7d08faa3428356786752aaa4b5a5d390
Alexmag75 <aleksandrmargrarit@gmail.com>
2025-12-10 18:51:32 +0200

    Commit start

---
+++ HWLesson1/Lesson7/hw8/script_task8.ts
@@ -0,0 +1,50 @@
+// let Cinderellas=function (name,age,footSize) {
+//     this.name=name;
+//     this.age=age;
+//     this.footSize=footSize;
+// }
+
+class Cinderellas{
+    name:string;
+    age:number;
+    footSize:number
+    constructor(name:string,age:number,footSize:number)
+    {
+        this.name=name;
+        this.age=age;
+        this.footSize=footSize;
+    }
+}
+class Princes {
+    name: string;
+    age: number;
+    shoeSize: number;
+    constructor(name:string,age:number,shoeSize:number) {
+        this.name=name;
+        this.age=age;
+        this.shoeSize=shoeSize;
+    }
+}
+
+let cinderella7_8:Cinderellas[]=[
+    new Cinderellas('Olga',18,32),
+    new Cinderellas('Tania',19,30),
+    new Cinderellas('Sveta',18,31),
+    new Cinderellas('Lida',20,36),
+    new Cinderellas('Vika',18,31),
+    new Cinderellas('Inga',17,32),
+    new Cinderellas('Rima',22,33),
+    new Cinderellas('Maria',21,29),
+    new Cinderellas('Masha',21,31),
+    new Cinderellas('Maia',18,35),
+    ]
+
+let prince = new Princes('Petia',25,30);
+for (const element of cinderella7_8) {
+    if (element.footSize===prince.shoeSize) {
+        console.log(element)
+    }
+}
+
+let findCinderella=cinderella7_8.find(value=>value.footSize===prince.shoeSize)
+console.log(findCinderella);
